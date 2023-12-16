c789592ae5a298d7da890518d05bfadc3cc09fff
Clara Battesini <clarabattesini@gmail.com>
2023-12-15 22:02:02 -0500

    New task function

--- task-manager/packages/task/task.go
+++ task-manager/packages/task/task.go
@@ -1,6 +1,11 @@
 package task
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 type TaskStatus int
 type Priority int
@@ -8,7 +13,7 @@ type Priority int
 type Task struct {
 	ID          int
 	Name        string
-	DueDate     string
+	DueDate     time.Time
 	Status      TaskStatus
 	Description string
 	Recurrent   bool
@@ -27,13 +32,45 @@ const (
 	Done
 )
 
+func intToTaskStatus(i int) (TaskStatus, error) {
+	switch i {
+	case 0:
+		return Todo, nil
+	case 1:
+		return InProgress, nil
+	case 2:
+		return Done, nil
+	default:
+		return 0, fmt.Errorf("unknown TaskStatus for value: %d", i)
+	}
+}
+
 func NewTask(record []string) (*Task, error) {
+	dateParsed, err := time.Parse("2006-01-02", record[1])
+	if err != nil {
+		panic(err)
+	}
+
+	intStatus, err := strconv.Atoi(record[2])
+	if err != nil {
+		panic(err)
+	}
+	status, err := intToTaskStatus(intStatus)
+
+	recurrent, err := strconv.ParseBool(record[4])
+	if err != nil {
+		panic(err)
+	}
+
+	randomId := rand.Intn(900) + 100
+
 	return &Task{
-		Name:        "mock",
-		DueDate:     "mock",
-		Status:      Todo,
-		Description: "mock",
-		Recurrent:   false,
+		ID:          randomId,
+		Name:        record[0],
+		DueDate:     dateParsed,
+		Status:      status,
+		Description: record[3],
+		Recurrent:   recurrent,
 		Priority:    High,
 	}, nil
 }
